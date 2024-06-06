f12b48b9f94b692063df7a6828e6e196a914fb82
Leandro <leandroenardi@gmail.com>
2024-06-06 20:19:09 -0300

    Inicio autenticacion

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { iAuthRequest } from '../interfaces/auth';
+import { Session } from '../interfaces/session';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +12,99 @@ export class AuthenticationService {
     private backendUrl: string = environment.endpoint;
 
   constructor() { }
+
+  
+  private loggedIn: boolean = false;
+
+  async login(authentication:iAuthRequest): Promise<boolean> {
+    const res = await fetch(this.backendUrl + 'api/authentication/authenticate', {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(authentication),
+    });
+    if(!res.ok) return false
+    const token = await res.text();
+    console.log(token)
+
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken(token);
+    console.log(decodedToken)
+    const sub = decodedToken.sub;
+    const role = decodedToken.roles;
+
+    if (!token) return false;
+    this.setSession(token);
+    this.setUserId(sub);
+    this.setUserRole(role);
+    this.setIsLoggedIn();
+    return true;
+  }
+
+  isLoggedIn(){
+    return localStorage.getItem('IsLoggedIn') == "true";
+  }
+
+  getSession(): Session {
+    const item: string = localStorage.getItem('session') || 'invalid';
+    if (item !== 'invalid') {
+      this.loggedIn = true;
+      return JSON.parse(item);
+    }
+    return { expiresIn: '', token: '' };
+  }
+
+
+  setUserId(id : string){
+    localStorage.setItem('Id', id);
+  }
+
+  setUserRole(role : string){
+    localStorage.setItem('Role', role);
+  }
+
+  setIsLoggedIn(){
+    localStorage.setItem('IsLoggedIn', "true");
+  }
+
+  getUserRole(){
+    return localStorage.getItem('Role');
+  }
+
+  getUserId(){
+    return localStorage.getItem('Id');
+  }
+
+  isAdmin(){
+    return this.getUserRole() == 'Administrador'
+  }
+
+  setSession(token: any, expiresTimeHours: number = 1) {
+    const date = new Date();
+    date.setHours(date.getHours() + expiresTimeHours);
+
+    const session: Session = {
+      expiresIn: new Date(date).toISOString(),
+      token,
+    };
+
+    this.loggedIn = true;
+    localStorage.setItem('session', JSON.stringify(session));
+  }
+
+  async getMe() {
+    const res = await fetch('', {
+      headers: {
+        Authorization: this.getSession().token!,
+      },
+    });
+    return await res.json();
+  }
+
+  resetSession() {
+    localStorage.removeItem('session');
+    localStorage.removeItem('Id');
+    localStorage.removeItem('Role');
+    localStorage.removeItem('IsLoggedIn');
+    this.loggedIn = false;
+  }
 }
