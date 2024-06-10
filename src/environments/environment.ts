0f2831829dd87f2b5587249d3431712a83c68352
Leandro <leandroenardi@gmail.com>
2024-06-09 21:24:51 -0300

    Environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    endpoint: 'ec2-54-234-47-225.compute-1.amazonaws.com:8080/',
+    endpoint: 'http://ec2-34-203-242-66.compute-1.amazonaws.com:8080/',
 };
